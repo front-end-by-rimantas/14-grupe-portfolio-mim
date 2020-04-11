ec07d6b89ed2a13027e7db996df677aad28ff52d
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-04-11 21:39:06 +0300

    about: first draft

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-import from './reset.js';
-import from'./main.js';
-import from'./main.js';
-import from'./header.js';
-import from'./introduction.js';
-import from'./about.js';
-import from'./experience.js';
-import from'./portfolio.js';
-import from'./services.js';
-import from'./testimonials.js';
-import from'./blog.js';
-import from'./contacts.js';
-import from'./site-navigation.js';
-import from'./side-menu.js';
-import from'./footer.js';
+// import from './reset.js';
+// import from'./header.js';
+// import from'./introduction.js';
+
+// import from'./about.js';
+
+// import from'./experience.js';
+// import from'./portfolio.js';
+// import from'./services.js';
+// import from'./testimonials.js';
+// import from'./blog.js';
+// import from'./contacts.js';
+// import from'./site-navigation.js';
+// import from'./side-menu.js';
+// import from'./footer.js';
