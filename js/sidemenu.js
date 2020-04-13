b565a4d527d3a5e66af8f8bd9349a2ab571bd606
teklithuania <61139751+teklithuania@users.noreply.github.com>
2020-04-13 17:11:04 +0300

    Navigation logic

--- js/sidemenu.js
+++ js/sidemenu.js
@@ -13,4 +13,24 @@ return sideMenu.classList.remove("show-side-menu");
 
 });
 
+function renderElement( sideList ) {
+    let HTML = '';
+
+    if ( !Array.isArray(sideList) ) {
+        return console.log('ERROR: data is not arrey..');  
+    }
+    if ( sideList.length === 0 ) {
+        return console.log('ERROR: arrey cannot be empty');  
+    }
+
+    for (let i = 0; i < sideList.length; i++) {
+        const sideSection = sideList[i];
+        HTML += `<div class="${sideSection.class}">
+                    <a href="${sideSection.id}">${sideSection.description}</a>
+                </div>`;
+    }
+    return document.querySelector('#transferSideMenu').innerHTML = HTML;
+}
+renderElement(side);
+
 export default sideMenu;
