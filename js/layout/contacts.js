9170272de325b6533ed9f0611d27f7e9513021a0
ViktorasZigaras <44396727+ViktorasZigaras@users.noreply.github.com>
2020-04-22 18:34:03 +0300

    merge fix

--- js/layout/contacts.js
+++ js/layout/contacts.js
@@ -12,63 +12,58 @@ function renderContacts(contactData){
                 <h4>${contactData[i].h4}</h4>
                 <a href="#" class = ${contactData[i].class}>${contactData[i].atext}</a>
                 </div>
-                </div>
-               
-                `
+                </div>`
     }
     let contactmenu = document.querySelector('.bottom');
-    console.log(contactmenu)
     return contactmenu.innerHTML = HTML; 
 }
 
-export default function setUpContacts() {
-    renderContacts(contacts)
-    console.log(document.querySelector('.cont-btn'))
+
     document.querySelector('.cont-btn').addEventListener('click', renderAlert);
-}
 
-function renderAlert() {
+    function renderAlert(){
+        const textAlert = document.forms["form"]['text'].value;
+        const emailAlert = document.forms["form"]['email'].value;
+        const textareaAlert = document.querySelector('textarea').value;
 
-    const textAlert = document.forms["form"]['text'].value;
-    const emailAlert = document.forms["form"]['email'].value;
-    const textareaAlert = document.querySelector('textarea').value;
-
-    console.log(document.querySelector(".alert-text"))
-
-    if ( textAlert === '' ) {
-        document.querySelector(".alert-text").innerHTML = 'The field is required.';
-    }
-
-    let email = 0;
-    for ( let i=0; i<emailAlert.length; i++ ) {
-        if ( emailAlert[i] === '@' ) {
-            email++;
-        }
-    }
-
-    if ( textareaAlert === '' ) {
-        document.querySelector(".alert-textarea").innerHTML = 'The field is required.';
-    }
-
-    if ( 
-        emailAlert[0] === '@' ||
-        emailAlert.length-1 === '@' ||
-        (emailAlert.length < 5) ) {
-            document.querySelector(".alert-email").innerHTML = 'The e-mail address entered is invalid.';
+        if ( textAlert === '' ) {
+            document.querySelector(".alert-text").innerHTML = 'The field is required.';
         }
 
-    if ( emailAlert === '' ) {
-        document.querySelector(".alert-email").innerHTML = 'The field is required.';
+        let email = 0;
+        for ( let i=0; i<emailAlert.length; i++ ) {
+            if ( emailAlert[i] === '@' ) {
+                email++;
+            }
+        }
+
+        if ( textareaAlert === '' ) {
+            document.querySelector(".alert-textarea").innerHTML = 'The field is required.';
+        }
+
+        if ( 
+            emailAlert[0] === '@' ||
+            emailAlert.length-1 === '@' ||
+            (emailAlert.length < 5) ) {
+                document.querySelector(".alert-email").innerHTML = 'The e-mail address entered is invalid.';
+            }
+
+        if ( emailAlert === '' ) {
+            document.querySelector(".alert-email").innerHTML = 'The field is required.';
+        }
+        if ( textareaAlert === '' || emailAlert === '' || textareaAlert === '' || emailAlert.length < 5 ||
+            emailAlert[0] === '@' ||
+            emailAlert.length-1 === '@'){
+            return document.querySelector(".alert-btn").innerHTML = 'One or more fields have an error. Please check and try again.';
+        }
+      
+        return
+    
     }
-    if ( textareaAlert === '' || emailAlert === '' || textareaAlert === '' || emailAlert.length < 5 ||
-        emailAlert[0] === '@' ||
-        emailAlert.length-1 === '@'){
-        return document.querySelector(".alert-btn").innerHTML = 'One or more fields have an error. Please check and try again.';
+
+    function setUpContacts() {
+        renderContacts(contacts)
+
     }
     
-    return
-
-}
-
-   
-
+    export default setUpContacts;
